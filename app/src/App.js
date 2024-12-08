fd12ad2d65cac8d4f1fcab7596683be98810b0c7
ZekePulido <ezekielpulido10703@hotmail.com>
2024-12-07 20:38:22 -0600

    Added Student Login

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { HomePage } from './Components/HomePage/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { StudentLogin } from './Components/StudentLogin/StudentLogin';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <StudentLogin/>
     </div>
   );
 }
