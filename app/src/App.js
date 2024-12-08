fd98f77500e710296df97932eb81af31ad57d50e
ClaireG-J <107225401+ClaireG-J@users.noreply.github.com>
2024-12-08 15:05:52 -0600

    Update App.js to add routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { HomePage } from './Components/HomePage/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
 import { StudentLogin } from './Components/StudentLogin/StudentLogin';
+import Layout from "./Components/Layout";
 
-function App() {
+export default function App() {
   return (
     <div>
-      <StudentLogin/>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="studentlogin" element={<StudentLogin />} />
+        </Route>
+      </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
