6cea859f20a15b1a254a6fabd9fb8512e229eed3
Arh3825 <alisonho38@gmail.com>
2025-01-30 18:04:56 -0600

    Revert "updated frontend pages"
    This reverts commit cde25a492e81a2dea7be4cc1eb23823bdf67b41f.

--- app/src/Components/pages/TeacherDashboard/TeacherDashboard.jsx
+++ app/src/Components/pages/TeacherDashboard/TeacherDashboard.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import styles from './TeacherDashboard.module.css';
+import { useNavigate } from "react-router-dom";
 
 export const TeacherDashboard = () => {
+    const navigate = useNavigate();
+  const toScore = () => {
+    navigate('/teacherscore');
+};
   return (
     <div className={styles.pageWrapper}>
       <div className={styles.topRow}>
         <div className={styles.name}>Hello Ms./Mr. _________</div>
         <div className={styles.codeWrapper}>
           <div className={styles.codeTitle}>This is your Teacher Code:</div>
-          <div className={styles.code}>3frv4e</div>
+          <div className={styles.code}>3dic4e</div>
         </div>
         <div className={styles.gradePrompt}>Select the grade that you would like to view</div>
       </div>
 
       <div className={styles.bottomRow}>
         <div className={styles.buttons}>
-          <div className={styles.button}>Grade 3</div>
+          <div className={styles.button} onClick={toScore}>Grade 3</div>
           <div className={styles.button}>Grade 4</div>
           <div className={styles.button}>Grade 5</div>
         </div>
