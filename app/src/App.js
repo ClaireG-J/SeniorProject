ba3ffeaa3a5d2b1a1eb4c1e819690e942d9fa2ee
ClaireG-J <107225401+ClaireG-J@users.noreply.github.com>
2025-05-09 16:48:31 -0500

    Update App.js
    Trying to fix routing on deployment

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { HomePage } from './Components/pages/HomePage/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from "react-dom/client";
+import { Layout } from './Components/Layout';
 import { StudentLogin } from './Components/pages/StudentLogin/StudentLogin';
 import { TeacherLogin } from './Components/pages/TeacherLogin/TeacherLogin';
 import { TeacherSignup } from './Components/pages/TeacherSignup/TeacherSignup';
@@ -17,17 +18,18 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/studentlogin" element={<StudentLogin/>} />
-        <Route path="/teacherlogin" element={<TeacherLogin/>} />
-        <Route path="/signup" element={<TeacherSignup/>} />
-        <Route path="/scorereport" element={< ScoreReport/>} />
-        <Route path="/answer" element={<StudentAnswer/>} />
-        <Route path="/question" element={<StudentQuestion/>} />
-        <Route path="/dashboard" element={<TeacherDashboard/>} />
-        <Route path="/teacherscore" element={<StudentScore/>} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:uid/:token" element={<ResetPassword/>} />
+        <Route path="/" element={<Layout />} />
+        <Route index element={<HomePage />} />
+        <Route path="studentlogin" element={<StudentLogin/>} />
+        <Route path="teacherlogin" element={<TeacherLogin/>} />
+        <Route path="signup" element={<TeacherSignup/>} />
+        <Route path="scorereport" element={< ScoreReport/>} />
+        <Route path="answer" element={<StudentAnswer/>} />
+        <Route path="question" element={<StudentQuestion/>} />
+        <Route path="dashboard" element={<TeacherDashboard/>} />
+        <Route path="teacherscore" element={<StudentScore/>} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="reset-password/:uid/:token" element={<ResetPassword/>} />
       </Routes>
     </Router>
   );
