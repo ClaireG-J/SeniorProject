7a59eb1784d41069bc065e94d23142f980830170
ZekePulido <ezekielpulido10703@hotmail.com>
2025-05-16 13:43:10 -0500

    Updated url

--- app/src/Components/pages/StudentLogin/StudentLogin.jsx
+++ app/src/Components/pages/StudentLogin/StudentLogin.jsx
@@ -22,7 +22,7 @@ export const StudentLogin = () => {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/student-login/', {
+      const response = await fetch('https://ila1.pythonanywhere.com/api/student-login/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ 
